cc85e61e9844411fc6343083254af8f080414222
TIMOTHY MARSH <timothy_marsh@epam.com>
2023-09-15 17:14:16 +0100

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHInheritanceTree
+{
+    public class Startup
+    {
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                      .SetBasePath(Directory.GetCurrentDirectory())
+                      .AddJsonFile("appsettings.json", optional: false);
+
+            IConfiguration config = builder.Build();
+
+            ApiSettings = config.GetSection("ApiSettings").Get<ApiSettings>();
+
+        }
+
+        public ApiSettings ApiSettings { get; private set; }
+    }
+}
